499386f760a4159d005f6a58621a9ec20038cde8
Kimgion <phatbo13579@gmail.com>
2025-03-11 20:56:31 +0700

    add feature download graph

--- assets/js/main.js
+++ assets/js/main.js
@@ -374,16 +374,19 @@ Nếu không có chu trình âm, trả về dist (mảng khoảng cách ngắn n
     selectElement.dispatchEvent(new Event("change"));
 });
 
+document.getElementById("capture_btn").addEventListener("click", function() {
+    if (!cy || cy.elements().length === 0) {
+        alert("Không có đồ thị!");
+        return;
+    }
 
+    const pngData = cy.png({ bg: 'white', full: true });
 
-// document.getElementById("convertToImageButton").addEventListener("click", function() {
-//     const pngData = cy.png({
-//         bg: 'white'
-//     });
-//     const link = document.createElement('a');
-//     link.href = pngData;
-//     link.download = 'graph.png';
-//     document.body.appendChild(link);
-//     link.click();
-//     document.body.removeChild(link);
-// });
+    const link = document.createElement('a');
+    link.href = pngData;
+    link.download = 'graph.png';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+});
+
