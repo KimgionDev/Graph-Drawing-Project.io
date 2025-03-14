b8514225e21dd9f8f8d5f76ae81abbed32358b75
Phuc <trantrongphuc1501@gmail.com>
2025-03-14 15:17:34 +0700

    Reconstruction

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,3 @@
-//Loading animation
-window.addEventListener("load", function () {
-    let loadingScreen = document.getElementById("loading-screen");
-
-    setTimeout(() => {
-        loadingScreen.style.transition = "opacity 0.6s ease";
-        loadingScreen.style.opacity = "0";
-    }, 200);
-
-    setTimeout(() => {
-        loadingScreen.style.display = "none";
-    }, 600);
-});
-
 //Change Theme
 const themeToggle = document.querySelector(".themeInp");
 
@@ -386,19 +372,4 @@ Nếu không có chu trình âm, trả về dist (mảng khoảng cách ngắn n
     selectElement.dispatchEvent(new Event("change"));
 });
 
-document.getElementById("capture_btn").addEventListener("click", function() {
-    if (!cy || cy.elements().length === 0) {
-        alert("Không có đồ thị!");
-        return;
-    }
-
-    const pngData = cy.png({ bg: 'white', full: true, scale: 2 });
-
-    const link = document.createElement('a');
-    link.href = pngData;
-    link.download = 'graph.png';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-});
 
