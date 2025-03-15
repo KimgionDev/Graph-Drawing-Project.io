b4a14530058cdb478ffb3519334e7a72d0530bbf
Phuc <trantrongphuc1501@gmail.com>
2025-03-15 15:14:50 +0700

    Fixed Theme

--- assets/js/changeTheme.js
+++ assets/js/changeTheme.js
@@ -1,6 +1,11 @@
-//Change Theme
-const themeToggle = document.querySelector(".themeInp");
+// //Change Theme
+// const themeToggle = document.querySelector(".themeInp");
 
-themeToggle.addEventListener('change', () => {
-    document.body.classList.toggle('light_theme_var');
-})
+// themeToggle.addEventListener('change', () => {
+//     document.body.classList.toggle('light_theme_var');
+// })
+function toggleTheme() {
+    document.body.classList.toggle("light_theme_var");
+    document.body.classList.toggle("dark_theme_var");
+}
+  
